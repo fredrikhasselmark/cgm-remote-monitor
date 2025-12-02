ab6b520d44b9d6aab6e49821ecd0550d493204c2
fredrik.hasselmark@gmail.com <fredrik.hasselmark@gmail.com>
2025-12-02 11:55:27 +0100

    Changed bundle output AND Refactor webpack configuration for consistent quotation style

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,9 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 const pluginArray = [];
-const sourceMapType = 'source-map';
-const MomentTimezoneDataPlugin = require('moment-timezone-data-webpack-plugin');
-const projectRoot = path.resolve(__dirname, '..');
+const sourceMapType = "source-map";
+const MomentTimezoneDataPlugin = require("moment-timezone-data-webpack-plugin");
+const projectRoot = path.resolve(__dirname, "..");
 
 /*
 if (process.env.NODE_ENV === 'development') {
@@ -45,94 +45,104 @@ if (process.env.NODE_ENV === 'development') {
 }
 */
 
-pluginArray.push(new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  'window.jQuery': 'jquery',
-  'window.$': 'jquery'
-}));
+pluginArray.push(
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    "window.jQuery": "jquery",
+    "window.$": "jquery",
+  })
+);
 
-pluginArray.push(new webpack.ProvidePlugin({
-  process: 'process/browser',
-}));
+pluginArray.push(
+  new webpack.ProvidePlugin({
+    process: "process/browser",
+  })
+);
 
 // limit Timezone data from Moment
 
-pluginArray.push(new MomentTimezoneDataPlugin({
-  startYear: 2015,
-  endYear: 2035,
-}));
+pluginArray.push(
+  new MomentTimezoneDataPlugin({
+    startYear: 2015,
+    endYear: 2035,
+  })
+);
 
-if (process.env.NODE_ENV === 'development') {
-  const ESLintPlugin = require('eslint-webpack-plugin');
-  pluginArray.push(new ESLintPlugin({
-    emitWarning: true,
-    failOnError: false,
-    failOnWarning: false,
-    formatter: require('eslint').CLIEngine.getFormatter('stylish'),
-    overrideConfig: {
-      globals: {
-        '$': 'writeable'
-      }
-    }
-  }));
+if (process.env.NODE_ENV === "development") {
+  const ESLintPlugin = require("eslint-webpack-plugin");
+  pluginArray.push(
+    new ESLintPlugin({
+      emitWarning: true,
+      failOnError: false,
+      failOnWarning: false,
+      formatter: require("eslint").CLIEngine.getFormatter("stylish"),
+      overrideConfig: {
+        globals: {
+          $: "writeable",
+        },
+      },
+    })
+  );
 }
 
 const rules = [
   {
     test: /\.(js|jsx)$/,
     use: {
-      loader: 'babel-loader',
+      loader: "babel-loader",
       options: {
         babelrc: true,
         cacheDirectory: true,
-        extends: path.join(projectRoot, '/.babelrc')
-      }
-    }
+        extends: path.join(projectRoot, "/.babelrc"),
+      },
+    },
   },
   {
     test: /\.css$/i,
-    use: [ 'style-loader',
+    use: [
+      "style-loader",
       {
-        loader: 'css-loader',
+        loader: "css-loader",
         options: {
           sourceMap: true,
         },
-      } ],
-    exclude: /node_modules/
+      },
+    ],
+    exclude: /node_modules/,
   },
   {
     test: /\.(jpe?g|png|gif)$/i,
-    loader: 'file-loader',
+    loader: "file-loader",
     options: {
-      outputPath: 'images'
+      outputPath: "images",
       //the images will be emitted to public/assets/images/ folder
       //the images will be put in the DOM <style> tag as eg. background: url(assets/images/image.png);
     },
-    exclude: /node_modules/
+    exclude: /node_modules/,
   },
   {
-    test: require.resolve('jquery'),
-    loader: 'expose-loader',
+    test: require.resolve("jquery"),
+    loader: "expose-loader",
     options: {
-      exposes: ['$']
-    }
-  }
+      exposes: ["$"],
+    },
+  },
 ];
 
-const appEntry = ['./bundle/bundle.source.js'];
-const clockEntry = ['./bundle/bundle.clocks.source.js'];
+const appEntry = ["./bundle/bundle.source.js"];
+const clockEntry = ["./bundle/bundle.clocks.source.js"];
 
-let mode = 'production';
-let publicPath = '/bundle/';
+let mode = "production";
+let publicPath = "/bundle/";
 
-if (process.env.NODE_ENV === 'development') {
-  mode = 'development';
-  publicPath = '/devbundle/';
+if (process.env.NODE_ENV === "development") {
+  mode = "development";
+  publicPath = "/devbundle/";
   pluginArray.push(new webpack.HotModuleReplacementPlugin());
   pluginArray.push(new webpack.NoEmitOnErrorsPlugin());
 
-  const hot = 'webpack-hot-middleware/client?port=1337';
+  const hot = "webpack-hot-middleware/client?port=1337";
 
   appEntry.unshift(hot);
   clockEntry.unshift(hot);
@@ -140,35 +150,34 @@ if (process.env.NODE_ENV === 'development') {
 
 const optimization = {};
 
-
 module.exports = {
   mode,
   context: projectRoot,
   entry: {
     app: appEntry,
-    clock: clockEntry
+    clock: clockEntry,
   },
   output: {
-    path: path.resolve(projectRoot, './node_modules/.cache/_ns_cache/public'),
+    path: path.resolve(projectRoot, "./release"), //'./node_modules/.cache/_ns_cache/public'),
     publicPath,
-    filename: 'js/bundle.[name].js',
-    sourceMapFilename: 'js/bundle.[name].js.map',
+    filename: "js/bundle.[name].js",
+    sourceMapFilename: "js/bundle.[name].js.map",
   },
   devtool: sourceMapType,
   optimization,
   plugins: pluginArray,
   module: {
-    rules
+    rules,
   },
   resolve: {
     fallback: {
-      'process/browser': require.resolve('process/browser'),
-      events: require.resolve('events/')
+      "process/browser": require.resolve("process/browser"),
+      events: require.resolve("events/"),
     },
     alias: {
-      stream: 'stream-browserify',
-      crypto: 'crypto-browserify',
-      buffer: 'buffer',
-    }
-  }
+      stream: "stream-browserify",
+      crypto: "crypto-browserify",
+      buffer: "buffer",
+    },
+  },
 };
